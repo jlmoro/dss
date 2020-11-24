59ae25ff81580c8c4582cac6101f3a2248bf03db
Jose L. Moreno <jose0909@live.com>
2020-11-24 07:30:27 -0500

    creacion de ruta perfil

---
+++ resources/js/pages/perfil/PerfilUsuario.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="perfil-usuario">
+    <h4>Pagina para los datos de usuario</h4>
+  </section>
+</template>
+<script>
+export default {
+  name: "",
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.perfil-usuario{
+
+}
+</style>
